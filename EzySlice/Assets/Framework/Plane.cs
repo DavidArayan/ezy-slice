9c1acd4a5b85b06511ee00d5ee6e783431ef857d
David Arayan <david.arayan12@gmail.com>
2017-11-03 16:58:02 +1100

    - Added the sample Plane and Triangle structures.

--- EzySlice/Assets/Framework/Plane.cs
+++ EzySlice/Assets/Framework/Plane.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace EzySlice {
-	// TO-DO
+	/**
+	 * Represents a simple 3D Plane structure with a position
+	 * and direction which extends infinitely in its axis. This provides
+	 * an optimal structure for collision tests for the slicing framework.
+	 */
 	public struct Plane {
-		private Vector3 position;
-		private float dist;
+		private readonly Vector3 m_normal;
+		private readonly float m_dist;
+
+		public Plane(Vector3 pos, Vector3 norm) {
+			this.m_normal = norm;
+			this.m_dist = Vector3.Dot(norm, pos);
+		}
+
+		public Plane(Vector3 norm, float dot) {
+			this.m_normal = norm;
+			this.m_dist = dot;
+		}
+
+		public Vector3 normal {
+			get { return this.m_normal; }
+		}
+
+		public float dist {
+			get { return this.m_dist; }
+		}
 	}
 }
--- EzySlice/Assets/Framework/Triangle.cs
+++ EzySlice/Assets/Framework/Triangle.cs
@@ -3,16 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace EzySlice {
-	// TO-DO
+	/**
+	 * Represents a simple 3D Triangle structure with position
+	 * and UV map. The UV is required if the slicer needs
+	 * to recalculate the new UV position for texture mapping.
+	 */
 	public struct Triangle {
 		// the points which represent this triangle
-		private Vector3 pos_a;
-		private Vector3 pos_b;
-		private Vector3 pos_c;
+		private readonly Vector3 m_pos_a;
+		private readonly Vector3 m_pos_b;
+		private readonly Vector3 m_pos_c;
 
 		// the UV coordinates of this triangle
-		private Vector2 uv_a;
-		private Vector2 uv_b;
-		private Vector2 uv_c;
+		private readonly Vector2 m_uv_a;
+		private readonly Vector2 m_uv_b;
+		private readonly Vector2 m_uv_c;
+
+		public Triangle(Vector3 posa, 
+						Vector3 posb, 
+						Vector3 posc,
+						Vector2 uva, 
+						Vector2 uvb, 
+						Vector2 uvc) 
+		{
+			this.m_pos_a = posa;
+			this.m_pos_b = posb;
+			this.m_pos_c = posc;
+
+			this.m_uv_a = uva;
+			this.m_uv_b = uvb;
+			this.m_uv_c = uvc;
+		}
+
+		public Vector3 positionA {
+			get { return this.m_pos_a; }
+		}
+
+		public Vector3 positionB {
+			get { return this.m_pos_b; }
+		}
+
+		public Vector3 positionC {
+			get { return this.m_pos_c; }
+		}
+
+		public Vector2 uvA {
+			get { return this.m_uv_a; }
+		}
+
+		public Vector2 uvB {
+			get { return this.m_uv_b; }
+		}
+
+		public Vector2 uvC {
+			get { return this.m_uv_c; }
+		}
 	}
 }
