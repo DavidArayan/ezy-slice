1c48962d4fdeeb73efb4bd21e3622ed7c1238ecf
David Arayan <david.arayan12@gmail.com>
2017-11-03 16:36:00 +1100

    - Starting a refactor effort to remake the slicer - Adding more modern and performant code - Will be adding multi-threaded support - Will fix all remaining bugs and problems

---
+++ EzySlice/Assets/Framework/Plane.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EzySlice {
+	// TO-DO
+	public struct Plane {
+		private Vector3 position;
+		private float dist;
+	}
+}
---
+++ EzySlice/Assets/Framework/Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EzySlice {
+	// TO-DO
+	public struct Triangle {
+		// the points which represent this triangle
+		private Vector3 pos_a;
+		private Vector3 pos_b;
+		private Vector3 pos_c;
+
+		// the UV coordinates of this triangle
+		private Vector2 uv_a;
+		private Vector2 uv_b;
+		private Vector2 uv_c;
+	}
+}
