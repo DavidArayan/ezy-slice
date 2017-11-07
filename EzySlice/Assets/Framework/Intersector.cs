1490203c5ca12bd939e34cb0313aae4d7ee2fc6a
David Arayan <david.arayan12@gmail.com>
2017-11-08 10:01:29 +1100

    - Fixed some errors in Intersectior.cs - Overloaded Plane->Line Intersection - Changed "ref" for "out" parameters

--- EzySlice/Assets/Framework/Intersector.cs
+++ EzySlice/Assets/Framework/Intersector.cs
@@ -10,11 +10,18 @@ namespace EzySlice {
 		 * in reference q. Function returns true if intersection has been found or
 		 * false otherwise.
 		 */
-		public static sealed bool Intersect(Plane pl, Line ln, ref Vector3 q) {
-			const Vector3 a = ln.positionA;
-			const Vector3 b = ln.positionB;
-			const Vector3 normal = pl.normal;
-			const Vector3 ab = b - a;
+		public static bool Intersect(Plane pl, Line ln, out Vector3 q) {
+			return Intersector.Intersect(pl, ln.positionA, ln.positionB, out q);
+		}
+
+		/**
+		 * Perform an intersection between Plane and Line made up of points a and b. Intersection
+		 * point will be stored in reference q. Function returns true if intersection has been
+		 * found or false otherwise.
+		 */
+		public static bool Intersect(Plane pl, Vector3 a, Vector3 b, out Vector3 q) {
+			Vector3 normal = pl.normal;
+			Vector3 ab = b - a;
 
 			float t = (pl.dist - Vector3.Dot(normal, a)) / Vector3.Dot(normal, ab);
 
@@ -24,11 +31,9 @@ namespace EzySlice {
 				return true;
 			}
 
+			q = Vector3.zero;
+
 			return false;
-		}
-
-		public static sealed bool Intersect(Plane pl, Triangle tn, ref Triangle[] triangles) {
-
 		}
 	}
 }
