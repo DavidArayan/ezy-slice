9e794f12b6c41807a1f2e5fdf4669076dfa7c70e
David Arayan <david.arayan12@gmail.com>
2018-01-27 14:10:14 +1100

    - Slicer now has a new Mesh generator routine which works with submeshes. Submesh slicing will keep the original submesh and add one more for the cross section. - This change means that cross sections are no longer generated as child GameObjects but as Submeshes. An alternative API may be added for the GameObjecrt version in the future.

--- EzySlice/Slicer.cs
+++ EzySlice/Slicer.cs
@@ -6,6 +6,14 @@ namespace EzySlice {
 	public sealed class Slicer {
 
 		/**
+		 * An internal class for storing internal submesh values
+		 */ 
+		internal class SlicedSubmesh {
+			public readonly List<Triangle> upperHull = new List<Triangle>();
+			public readonly List<Triangle> lowerHull = new List<Triangle>();
+		}
+
+		/**
 		 * Helper function which will slice the provided object with the provided plane
 		 * and instantiate and return the final GameObjects
 		 * 
@@ -86,76 +94,205 @@ namespace EzySlice {
 
 			Vector3[] ve = sharedMesh.vertices;
 			Vector2[] uv = sharedMesh.uv;
-			int[] indices = sharedMesh.triangles;
 
-			int indicesCount = indices.Length;
+			int submeshCount = sharedMesh.subMeshCount;
+
+			// each submesh will be sliced and placed in its own array structure
+			SlicedSubmesh[] slices = new SlicedSubmesh[submeshCount];
+			// the cross section hull is common across all submeshes
+			List<Vector3> crossHull = new List<Vector3>();
 
 			// we reuse this object for all intersection tests
 			IntersectionResult result = new IntersectionResult();
 
-			// all our buffers, as Triangles
-			List<Triangle> upperHull = new List<Triangle>();
-			List<Triangle> lowerHull = new List<Triangle>();
-			List<Vector3> crossHull = new List<Vector3>();
+			// iterate over all the submeshes individually. vertices and indices
+			// are all shared within the submesh
+			for (int submesh = 0; submesh < submeshCount; submesh++) {
+				int[] indices = sharedMesh.GetTriangles(submesh);
+				int indicesCount = indices.Length;
 
-			// loop through all the mesh vertices, generating upper and lower hulls
-			// and all intersection points
-			for (int index = 0; index < indicesCount; index += 3) {
-				int i0 = indices[index + 0];
-				int i1 = indices[index + 1];
-				int i2 = indices[index + 2];
+				SlicedSubmesh mesh = new SlicedSubmesh();
 
-				Triangle newTri = new Triangle(ve[i0], ve[i1], ve[i2], uv[i0], uv[i1], uv[i2]);
+				// loop through all the mesh vertices, generating upper and lower hulls
+				// and all intersection points
+				for (int index = 0; index < indicesCount; index += 3) {
+					int i0 = indices[index + 0];
+					int i1 = indices[index + 1];
+					int i2 = indices[index + 2];
 
-				// slice this particular triangle with the provided
-				// plane
-				if (newTri.Split(pl, result)) {
-					int upperHullCount = result.upperHullCount;
-					int lowerHullCount = result.lowerHullCount;
-					int interHullCount = result.intersectionPointCount;
+					Triangle newTri = new Triangle(ve[i0], ve[i1], ve[i2], uv[i0], uv[i1], uv[i2]);
 
-					for (int i = 0; i < upperHullCount; i++) {
-						upperHull.Add(result.upperHull[i]);
-					}
+					// slice this particular triangle with the provided
+					// plane
+					if (newTri.Split(pl, result)) {
+						int upperHullCount = result.upperHullCount;
+						int lowerHullCount = result.lowerHullCount;
+						int interHullCount = result.intersectionPointCount;
 
-					for (int i = 0; i < lowerHullCount; i++) {
-						lowerHull.Add(result.lowerHull[i]);
-					}
+						for (int i = 0; i < upperHullCount; i++) {
+							mesh.upperHull.Add(result.upperHull[i]);
+						}
 
-					for (int i = 0; i < interHullCount; i++) {
-						crossHull.Add(result.intersectionPoints[i]);
-					}
-				}
-				else {
-					SideOfPlane side = pl.SideOf(ve[i0]);
+						for (int i = 0; i < lowerHullCount; i++) {
+							mesh.lowerHull.Add(result.lowerHull[i]);
+						}
 
-					if (side == SideOfPlane.UP || side == SideOfPlane.ON) {
-						upperHull.Add(newTri);
+						for (int i = 0; i < interHullCount; i++) {
+							crossHull.Add(result.intersectionPoints[i]);
+						}
 					}
 					else {
-						lowerHull.Add(newTri);
+						SideOfPlane side = pl.SideOf(ve[i0]);
+
+						if (side == SideOfPlane.UP || side == SideOfPlane.ON) {
+							mesh.upperHull.Add(newTri);
+						}
+						else {
+							mesh.lowerHull.Add(newTri);
+						}
 					}
 				}
+
+				// register into the index
+				slices[submesh] = mesh;
 			}
 
-			// we don't want cross sections, 
-			if (!genCrossSection) {
-				// start creating our hulls
-				Mesh finalUpperHullNoCross = CreateFrom(upperHull, null);
-				Mesh finalLowerHullNoCross = CreateFrom(lowerHull, null);
+			return CreateFrom(slices, CreateFrom(crossHull, pl.normal));
+		}
 
-				return new SlicedHull(finalUpperHullNoCross, finalLowerHullNoCross);
+		/**
+		 * Generates a single SlicedHull from a set of cut submeshes 
+		 */
+		private static SlicedHull CreateFrom(SlicedSubmesh[] meshes, List<Triangle> cross) {
+			int submeshCount = meshes.Length;
+
+			int upperHullCount = 0;
+			int lowerHullCount = 0;
+
+			// get the total amount of upper, lower and intersection counts
+			for (int submesh = 0; submesh < submeshCount; submesh++) {
+				upperHullCount += meshes[submesh].upperHull.Count;
+				lowerHullCount += meshes[submesh].lowerHull.Count;
 			}
 
-			// we need to generate the cross section if set
-			// NOTE -> This uses a MonotoneChain algorithm which will only work
-			// on cross sections which are Convex
-			List<Triangle> crossSection = CreateFrom(crossHull, pl.normal);
+			Mesh upperHull = CreateHull(meshes, upperHullCount, cross, true);
+			Mesh lowerHull = CreateHull(meshes, lowerHullCount, cross, false);
 
-			Mesh finalUpperHull = CreateFrom(upperHull, crossSection, true);
-			Mesh finalLowerHull = CreateFrom(lowerHull, crossSection, false);
+			return new SlicedHull(upperHull, lowerHull);
+		}
 
-			return new SlicedHull(finalUpperHull, finalLowerHull);
+		/**
+		 * Generate a single Mesh HULL of either the UPPER or LOWER hulls. 
+		 */
+		private static Mesh CreateHull(SlicedSubmesh[] meshes, int total, List<Triangle> crossSection, bool isUpper) {
+			if (total <= 0) {
+				return null;
+			}
+
+			int submeshCount = meshes.Length;
+			int crossCount = crossSection != null ? crossSection.Count : 0;
+
+			Mesh newMesh = new Mesh();
+
+			// vertices and uv's are common for all submeshes
+			Vector3[] newVertices = new Vector3[(total + crossCount) * 3];
+			Vector2[] newUvs = new Vector2[(total + crossCount) * 3];
+
+			// each index refers to our submesh triangles
+			List<int[]> triangles = new List<int[]>(submeshCount);
+
+			int vIndex = 0;
+
+			// first we generate all our vertices, uv's and triangles
+			for (int submesh = 0; submesh < submeshCount; submesh++) {
+				// pick the hull we will be playing around with
+				List<Triangle> hull = isUpper ? meshes[submesh].upperHull : meshes[submesh].lowerHull;
+				int hullCount = hull.Count;
+
+				int[] indices = new int[hullCount * 3];
+
+				// fill our mesh arrays
+				for (int i = 0, triIndex = 0; i < hullCount; i++, triIndex += 3) {
+					Triangle newTri = hull[i];
+
+					int i0 = vIndex + 0;
+					int i1 = vIndex + 1;
+					int i2 = vIndex + 2;
+
+					newVertices[i0] = newTri.positionA;
+					newVertices[i1] = newTri.positionB;
+					newVertices[i2] = newTri.positionC;
+
+					newUvs[i0] = newTri.uvA;
+					newUvs[i1] = newTri.uvB;
+					newUvs[i2] = newTri.uvC;
+
+					// triangles are returned in clocwise order from the
+					// intersector, no need to sort these
+					indices[triIndex] = i0;
+					indices[triIndex + 1] = i1;
+					indices[triIndex + 2] = i2;
+
+					vIndex += 3;
+				}
+
+				// add triangles to the index for later generation
+				triangles.Add(indices);
+			}
+
+			// generate the cross section required for this particular hull
+			if (crossSection != null && crossCount > 0) {
+				int[] crossIndices = new int[crossCount * 3];
+
+				for (int i = 0, triIndex = 0; i < crossCount; i++, triIndex += 3) {
+					Triangle newTri = crossSection[i];
+
+					int i0 = vIndex + 0;
+					int i1 = vIndex + 1;
+					int i2 = vIndex + 2;
+
+					newVertices[i0] = newTri.positionA;
+					newVertices[i1] = newTri.positionB;
+					newVertices[i2] = newTri.positionC;
+
+					newUvs[i0] = newTri.uvA;
+					newUvs[i1] = newTri.uvB;
+					newUvs[i2] = newTri.uvC;
+
+					// add triangles in clockwise for upper
+					// and reversed for lower hulls, to ensure the mesh
+					// is facing the right direction
+					if (isUpper) {
+						crossIndices[triIndex] = i0;
+						crossIndices[triIndex + 1] = i1;
+						crossIndices[triIndex + 2] = i2;
+					}
+					else {
+						crossIndices[triIndex] = i0;
+						crossIndices[triIndex + 1] = i2;
+						crossIndices[triIndex + 2] = i1;
+					}
+
+					vIndex += 3;
+				}
+
+				// add triangles to the index for later generation
+				triangles.Add(crossIndices);
+			}
+
+			int totalTriangles = triangles.Count;
+
+			newMesh.subMeshCount = totalTriangles;
+			// fill the mesh structure
+			newMesh.vertices = newVertices;
+			newMesh.uv = newUvs;
+
+			// add the submeshes
+			for (int i = 0; i < totalTriangles; i++) {
+				newMesh.SetTriangles(triangles[i], i, false);
+			}
+
+			return newMesh;
 		}
 
 		/**
@@ -170,114 +307,6 @@ namespace EzySlice {
 			}
 
 			return null;
-		}
-
-		/**
-		 * Generate a mesh from the provided hull made of triangles
-		 * ADDED -> Generate the Cross Section into the same mesh
-		 */
-		private static Mesh CreateFrom(List<Triangle> hull, List<Triangle> crossSection, bool isUpper = true) {
-			int count = crossSection == null ? hull.Count : (hull.Count + crossSection.Count);
-
-			if (count <= 0) {
-				return null;
-			}
-
-			Mesh newMesh = new Mesh();
-
-			int hullCount = hull.Count;
-
-			Vector3[] newVertices = new Vector3[count * 3];
-			Vector2[] newUvs = new Vector2[count * 3];
-			int[] newIndices = new int[hullCount * 3];
-
-			int addedCount = 0;
-
-			// fill our mesh arrays
-			for (int i = 0; i < hullCount; i++) {
-				Triangle newTri = hull[i];
-
-				int i0 = addedCount + 0;
-				int i1 = addedCount + 1;
-				int i2 = addedCount + 2;
-
-				newVertices[i0] = newTri.positionA;
-				newVertices[i1] = newTri.positionB;
-				newVertices[i2] = newTri.positionC;
-
-				newUvs[i0] = newTri.uvA;
-				newUvs[i1] = newTri.uvB;
-				newUvs[i2] = newTri.uvC;
-
-				// triangles are returned in clocwise order from the
-				// intersector, no need to sort these
-				newIndices[i0] = i0;
-				newIndices[i1] = i1;
-				newIndices[i2] = i2;
-
-				addedCount += 3;
-			}
-
-			int[] crossIndices = null;
-
-			// also generate our cross sections
-			if (crossSection != null) {
-				int crossCount = crossSection.Count;
-				int crossIndex = 0;
-
-				crossIndices = new int[crossCount * 3];
-
-				for (int i = 0; i < crossCount; i++) {
-					Triangle newTri = crossSection[i];
-
-					int i0 = addedCount + 0;
-					int i1 = addedCount + 1;
-					int i2 = addedCount + 2;
-
-					newVertices[i0] = newTri.positionA;
-					newVertices[i1] = newTri.positionB;
-					newVertices[i2] = newTri.positionC;
-
-					newUvs[i0] = newTri.uvA;
-					newUvs[i1] = newTri.uvB;
-					newUvs[i2] = newTri.uvC;
-
-					// add triangles in clockwise for upper
-					// and reversed for lower hulls, to ensure the mesh
-					// is facing the right direction
-					if (isUpper) {
-						crossIndices[crossIndex] = i0;
-						crossIndices[crossIndex + 1] = i1;
-						crossIndices[crossIndex + 2] = i2;
-					}
-					else {
-						crossIndices[crossIndex] = i0;
-						crossIndices[crossIndex + 1] = i2;
-						crossIndices[crossIndex + 2] = i1;
-					}
-
-					addedCount += 3;
-					crossIndex += 3;
-				}
-			}
-
-			newMesh.subMeshCount = 2;
-			// fill the mesh structure
-			newMesh.vertices = newVertices;
-			newMesh.uv = newUvs;
-
-			// set the first group of triangles
-			newMesh.SetTriangles(newIndices, 0, false);
-
-			// set the cross indices if they exist
-			if (crossIndices != null) {
-				newMesh.SetTriangles(crossIndices, 1, false);
-			}
-
-			// consider computing this array externally instead
-			newMesh.RecalculateNormals();
-
-			return newMesh;
 		}
 	}
 }
