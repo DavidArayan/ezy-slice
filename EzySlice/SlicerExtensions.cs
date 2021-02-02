b98da2a7c2a5da76a79fc1b6e8eb93e5cce83ab9
David Arayan <david.arayan12@gmail.com>
2021-02-02 17:19:01 +1100

    - Finally fixed the issue related to slicing non-uniform scaled objects. (Issue #23)

--- EzySlice/SlicerExtensions.cs
+++ EzySlice/SlicerExtensions.cs
@@ -21,7 +21,11 @@ namespace EzySlice {
         public static SlicedHull Slice(this GameObject obj, Vector3 position, Vector3 direction, TextureRegion textureRegion, Material crossSectionMaterial = null) {
             Plane cuttingPlane = new Plane();
 
-            Vector3 refUp = obj.transform.InverseTransformDirection(direction);
+            Matrix4x4 mat = obj.transform.worldToLocalMatrix;
+            Matrix4x4 transpose = mat.transpose;
+            Matrix4x4 inv = transpose.inverse;
+
+            Vector3 refUp = inv.MultiplyVector(direction).normalized;
             Vector3 refPt = obj.transform.InverseTransformPoint(position);
 
             cuttingPlane.Compute(refPt, refUp);
@@ -51,7 +55,11 @@ namespace EzySlice {
         public static GameObject[] SliceInstantiate(this GameObject obj, Vector3 position, Vector3 direction, TextureRegion cuttingRegion, Material crossSectionMaterial = null) {
             EzySlice.Plane cuttingPlane = new EzySlice.Plane();
 
-            Vector3 refUp = obj.transform.InverseTransformDirection(direction);
+            Matrix4x4 mat = obj.transform.worldToLocalMatrix;
+            Matrix4x4 transpose = mat.transpose;
+            Matrix4x4 inv = transpose.inverse;
+
+            Vector3 refUp = inv.MultiplyVector(direction).normalized;
             Vector3 refPt = obj.transform.InverseTransformPoint(position);
 
             cuttingPlane.Compute(refPt, refUp);
